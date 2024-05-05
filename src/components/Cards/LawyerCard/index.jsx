2ac91f5a249c32a103346fba981108e78a66f811
Muhammad Ali <malishoaib.2001@gmail.com>
2024-05-06 00:15:38 +0500

    Book Lawyer button

--- src/components/Cards/LawyerCard/index.jsx
+++ src/components/Cards/LawyerCard/index.jsx
@@ -1,10 +1,10 @@
 import { Button } from '@mui/material'
 import React from 'react'
 
-export const LawyerCard = ({ item, book=false }) => {
+export const LawyerCard = ({ item, book = false }) => {
 
   return (
-    <div style={{ margin: '10px', padding: '10px', borderRadius: '20px', width: '220px', height: '200px', boxShadow: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px", display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+    <div style={{ margin: '10px', padding: '10px', borderRadius: '20px', width: '220px', height: '240px', boxShadow: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px", display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
       <h3 style={{ color: 'black', alignSelf: 'center' }}>{item.name}</h3>
       <p style={{ color: 'black' }}>Email : {item.email}</p>
       <p style={{ color: 'black' }}>Field : {item.field}</p>
