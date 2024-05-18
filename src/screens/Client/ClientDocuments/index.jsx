52bf410742ccc91d63cb46d7d834f3aa3b236139
Muhammad Ali <malishoaib.2001@gmail.com>
2024-05-19 00:54:05 +0500

    Documents screen Updated

--- src/screens/Client/ClientDocuments/index.jsx
+++ src/screens/Client/ClientDocuments/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../../components/Header'
+import { Documents } from '../../../components/Documents'
 
 export const ClientDocuments = () => {
   return (
     <div>
-       <Header title="Documents" />
+      <Header title="Documents" />
+      <Documents />
     </div>
   )
 }
